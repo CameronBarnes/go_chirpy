b649c2149833a01754353cac1a020de52968511b
Cameron Barnes <cameron_barnes@outlook.com>
2025-08-12 13:42:36 -0700

    Basic project setup and server behavior

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cameronbarnes/go_chirpy
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	mux.HandleFunc("/healthz", healthcheck)
+	server := http.Server{Handler: mux, Addr: ":8080"}
+	server.ListenAndServe()
+}
